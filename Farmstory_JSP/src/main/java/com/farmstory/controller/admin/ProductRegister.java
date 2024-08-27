fa207a93537d341dfdb5a2dbb26cf2ffa6350a79
woo <woo24465@gmail.com>
2024-08-27 15:26:20 +0900

    adminController

---
+++ Farmstory_JSP/src/main/java/com/farmstory/controller/admin/ProductRegister.java
@@ -0,0 +1,23 @@
+package com.farmstory.controller.admin;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/ProductRegister.do")
+public class ProductRegister extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/admin/ProductRegister.html");
+		dispatcher.forward(req, resp);
+		
+	}
+}
